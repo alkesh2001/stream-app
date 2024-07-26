eaba12a579e127928f49482dd71802f5ccfe0bcb
alkesh <alkesh@alkeshs-MacBook-Air.local>
2024-07-26 17:15:28 +0530

    some changes in backend for cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,6 +8,8 @@ const app = express()
 
 app.use(cors({
     origin : process.env.CORS_ORIGIN ,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: ['Content-Type', 'Authorization'],
     credentials : true
 }))
 
